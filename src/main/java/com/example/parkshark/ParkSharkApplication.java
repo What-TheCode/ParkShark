5f319b8342d72a703f25cf94f1b5a674b9c97a42
Alexander <alexandervanw@gmail.com>
2021-11-16 15:24:55 +0100

    Initial commit

---
+++ src/main/java/com/example/parkshark/ParkSharkApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkSharkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkSharkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parkshark/ParkSharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkSharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
