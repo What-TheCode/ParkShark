eae50881a7c8d46ceb82f7f34e11165dfc418285
Wouter Biront <wouterbiront@gmail.com>
2021-11-17 13:31:42 +0100

    Repository aangepast

--- src/main/java/com/example/parkshark/repository/DivisionRepository.java
+++ src/main/java/com/example/parkshark/repository/DivisionRepository.java
@@ -1,15 +1,7 @@
 package com.example.parkshark.repository;
 
 import com.example.parkshark.domain.Division;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-@Repository
 public interface DivisionRepository extends JpaRepository<Division,Integer> {
 }
