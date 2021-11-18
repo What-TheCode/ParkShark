51d8b4e0e0bc898a23347572053b4be27db8084c
Alexander <alexandervanw@gmail.com>
2021-11-18 11:06:14 +0100

    Create parkinglot with validation

---
+++ src/main/java/com/example/parkshark/exceptions/InvalidTelephoneException.java
@@ -0,0 +1,9 @@
+package com.example.parkshark.exceptions;
+
+public class InvalidTelephoneException extends RuntimeException{
+
+    public InvalidTelephoneException(String message) {
+        super(message);
+    }
+
+}
