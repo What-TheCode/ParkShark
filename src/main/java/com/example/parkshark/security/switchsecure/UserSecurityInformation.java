96f64c9a58c7408d4b5a8344087fed7257fe5d05
Robbert Schelfhout <robbert.schelfhout@gmail.com>
2021-11-18 11:19:55 +0100

    fixed security nog eens

---
+++ src/main/java/com/example/parkshark/security/switchsecure/UserSecurityInformation.java
@@ -0,0 +1,22 @@
+package com.example.parkshark.security.switchsecure;
+
+public class UserSecurityInformation {
+    private String name;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
