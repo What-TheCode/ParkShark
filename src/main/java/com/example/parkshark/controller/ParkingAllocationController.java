3b9449fd69f11f46b10ed98c2e9dc39e651dedb4
Wouter Biront <wouterbiront@gmail.com>
2021-11-19 11:13:23 +0100

    ParkingAllocationController toegevoegd

--- src/main/java/com/example/parkshark/controller/ParkingAllocationController.java
+++ src/main/java/com/example/parkshark/controller/ParkingAllocationController.java
@@ -1,4 +1,44 @@
 package com.example.parkshark.controller;
 
+import com.example.parkshark.domain.dto.parkingAllocation.ParkingAllocationDto;
+import com.example.parkshark.security.switchsecure.SecurityGuard;
+import com.example.parkshark.service.ParkingAllocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/allocation")
 public class ParkingAllocationController {
+
+    private final ParkingAllocationService parkingAllocationService;
+
+    @Autowired
+    public ParkingAllocationController(ParkingAllocationService parkingAllocationService) {
+        this.parkingAllocationService = parkingAllocationService;
+    }
+
+    @GetMapping(produces = "application/json")
+    @SecurityGuard(SecurityGuard.ApiUserRole.MANAGER)
+    @ResponseStatus(HttpStatus.OK)
+    public List<ParkingAllocationDto> getAllocations(@RequestParam(required = false) Boolean isActive) {
+        return null;
+        // return parkingAllocationService.getAll(isActive);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @SecurityGuard(SecurityGuard.ApiUserRole.MEMBER)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void startAllocation() {
+        // parkingAllocationService.startAllocation();
+    }
+
+    @PutMapping(consumes = "application/json", path = "/{id}")
+    @SecurityGuard(SecurityGuard.ApiUserRole.MEMBER)
+    @ResponseStatus(HttpStatus.OK)
+    public void stopAllocation(@PathVariable("id") int id) {
+        // parkingAllocationService.stopAllocation(id);
+    }
 }
