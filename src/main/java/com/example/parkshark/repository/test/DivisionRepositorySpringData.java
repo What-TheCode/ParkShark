8db39846dada17f215fdea42804e58842aa8df83
Ralph <ralphput@hotmail.com>
2021-11-19 15:50:49 +0100

    removes springdatajpaTest

--- src/main/java/com/example/parkshark/repository/test/DivisionRepositorySpringData.java
+++
@@ -1,7 +0,0 @@
-package com.example.parkshark.repository.test;
-
-import com.example.parkshark.domain.Division;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DivisionRepositorySpringData extends JpaRepository<Division,Integer> {
-}
